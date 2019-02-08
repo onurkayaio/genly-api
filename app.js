de8db92e1c910ab6b90e0a24507dbd8a128aa5b2
onur kaya <onurkaya244@gmail.com>
2019-02-08 22:20:45 +0300

    add cors middleware.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 var tumblrRouter = require('./routes/tumblr');
 var spotifyRouter = require('./routes/spotify');
-
 var app = express();
 
 var PORT = 8080;
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(cors());
 
 app.use('/tumblr', tumblrRouter);
 app.use('/spotify', spotifyRouter);
